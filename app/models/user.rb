1ed36d3b9899c243b7dfb3c69b9aec5342124385
Joe Schafer <joeschaferastula@gmail.com>
2018-07-22 20:12:57 -0500

    Active record relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :user_cryptos
+  has_many :cryptos, through: :user_cryptos
+
 end
