de96e2326c28c2a3ebd85079236ec8949ef2f8a6
Joe Schafer <joeschaferastula@gmail.com>
2018-07-23 09:01:52 -0500

    Crypto parse logic

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,12 +1,5 @@
 class CryptosController < ApplicationController
 
-  require 'net/http'
-
-  source = "https://min-api.cryptocompare.com/data/price?fsym=ADA&tsyms=BTC,USD,EUR"
-  resp = Net::HTTP.get_response(URI.parse(source))
-  data = resp.body
-  result = JSON.parse(data)
-
   def search
   end
 end
--- app/models/crypto.rb
+++ app/models/crypto.rb
@@ -1,4 +1,25 @@
 class Crypto < ApplicationRecord
+  require 'net/http'
+
   has_many :user_cryptos
   has_many :users, through: :user_cryptos
+
+  attr_accessor :can_be_added
+
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
+  def self.new_from_lookup(ticker_symbol)
+    source = "https://min-api.cryptocompare.com/data/top/exchanges/full?fsym=#{ticker_symbol}&tsym=USD"
+    resp = Net::HTTP.get_response(URI.parse(source))
+    data = resp.body
+    looked_up_crypto = JSON.parse(data)['Data']
+
+    return nil unless looked_up_crypto['CoinInfo']['FullName']
+    new_crypto = new(ticker: looked_up_crypto['CoinInfo']['Name'], name: looked_up_crypto['CoinInfo']['FullName'])
+    new_crypto.last_price = looked_up_crypto['AggregatedData']['Price']
+    new_crypto
+  end
+
 end
