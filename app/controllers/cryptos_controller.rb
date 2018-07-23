1999d72669264661f0faa4358a819e5db236cd69
Joe Schafer <joeschaferastula@gmail.com>
2018-07-23 09:25:35 -0500

    Cryptos controller search functionality

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,5 +1,15 @@
 class CryptosController < ApplicationController
 
   def search
+    if params[:stock]
+      @crypto ||= Crypto.new_from_lookup(params[:crypto])
+    end
+    if @crypto
+      @crypto.can_be_added = current_user.can_add_stock?(@crypto.ticker)
+
+      render json: @crypto, methods: [:can_be_added]
+    else
+      render status: 404, json: { response: 'No crypto exists for this symbol.' }
+    end
   end
 end
