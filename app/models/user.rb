14418c5c0370673db38025d7acd6e3868ebe8ebd
Joe Schafer <joeschaferastula@gmail.com>
2018-07-23 09:07:21 -0500

    user model logic for adding cryptos to portfolio

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,22 @@ class User < ApplicationRecord
   has_many :user_cryptos
   has_many :cryptos, through: :user_cryptos
 
+ def crypto_already_added?(ticker_symbol)
+    crypto = Crypto.find_by_ticker(ticker_symbol)
+    return false unless crypto
+    user_cryptos.where(crypto_id: crypto.id).exists?
+  end
+
+  def under_crypto_limit?
+    (user_cryptos.count < 10)
+  end
+
+  def can_add_crypto?(ticker_symbol)
+    under_crypto_limit? && !crypto_already_added?(ticker_symbol)
+  end
+
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
+  end
+
 end
