ce11a7b6d3c14d6548964fb90fe1ff877dd08189
Joe Schafer <joeschaferastula@gmail.com>
2018-07-28 10:16:00 -0500

    suggestions component added to search component

--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import Suggestions from 'components/Suggestions'
 
+const API_URL = "https:/" + "/min-api.cryptocompare.com/data/top/exchanges/full?fsym=#"
 
 class Search extends Component {
   state = {
-     query: '',
-     results: []
-   }
-
-  const apiUrl = "https:/" + "/min-api.cryptocompare.com/data/top/exchanges/full?fsym=#"
-
-  getInfo = () => {
-      axios.get(`${apiUrl}${this.state.query}&tsym=USD`)
-        .then(({ data }) => {
-          this.setState({
-            results: data.data
-          })
-        })
+    query: '',
+    results: []
   }
 
- handleInputChange = () => {
+  getInfo = () => {
+      axios.get(`${API_URL}${this.state.query}&tsym=USD`)
+      .then(({ data }) => {
+        this.setState({
+          results: data.data
+        })
+      })
+  }
+
+  handleInputChange = () => {
     this.setState({
       query: this.search.value
     }, () => {
@@ -27,6 +27,7 @@ class Search extends Component {
         if (this.state.query.length % 2 === 0) {
           this.getInfo()
         }
+      } else if (!this.state.query) {
       }
     })
   }
@@ -39,10 +40,11 @@ class Search extends Component {
           ref={input => this.search = input}
           onChange={this.handleInputChange}
         />
-        <p>{this.state.query}</p>
+        <Suggestions results={this.state.results} />
       </form>
     )
   }
 }
 
 export default Search
+
